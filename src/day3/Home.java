7898297ca2ae97d6681dae223ef14939c9cbbc99
Rufat <huseynovrufat.usa@gmail.com>
2022-08-19 01:45:09 -0400

    Car and home update

--- src/day3/Home.java
+++ src/day3/Home.java
@@ -4,4 +4,7 @@ public class Home {
     public static void main(String[] args) {
         System.out.println("Sweet sweet home");
     }
+    public static void backYard(){
+        System.out.println("Backyard");
+    }
 }
